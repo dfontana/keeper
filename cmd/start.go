537741cacbfa73b3b3693792d8e60e3cdbe2c8be
dfontana <dyfontana@gmail.com>
2019-08-16 21:45:02 -0400

    Rewrote start

--- cmd/start.go
+++ cmd/start.go
@@ -6,6 +6,9 @@ import (
 	"strings"
 
 	"github.com/spf13/viper"
+	"gopkg.in/src-d/go-git.v4"
+	gitConfig "gopkg.in/src-d/go-git.v4/config"
+	"gopkg.in/src-d/go-git.v4/plumbing"
 
 	"github.com/dfontana/keeper/util"
 	"github.com/spf13/cobra"
@@ -18,6 +21,18 @@ var startCmd = &cobra.Command{
 	Long: `Using the template in your config file, beings a new branch
 	after prompting for template values`,
 	Run: func(cmd *cobra.Command, args []string) {
+		path, err := os.Getwd()
+		if err != nil {
+			fmt.Println("Failed to get working directory", err)
+			os.Exit(0)
+		}
+
+		r, err := git.PlainOpen(path)
+		if err != nil {
+			fmt.Println("Failed to open repository", err)
+			os.Exit(0)
+		}
+
 		valid := false
 		prompts := viper.GetStringSlice("prompts")
 		values := []string{}
@@ -43,20 +58,54 @@ var startCmd = &cobra.Command{
 			template = strings.Replace(template, "#s#", value, 1)
 		}
 
-		ack := util.PromptBool(fmt.Sprintf("Checkout to %s?", template))
+		ack := util.PromptBool(fmt.Sprintf("Checkout to %s", template))
 		if !ack {
 			fmt.Println("Cancelled.")
 			os.Exit(0)
 		}
 
-		params := []string{
-			"git",
-			"checkout",
-			"-b",
-			template,
+		branch := fmt.Sprintf("refs/heads/%s", template)
+		b := plumbing.ReferenceName(branch)
+		worktree, err := r.Worktree()
+		if err != nil {
+			fmt.Println("Failed to open worktree")
+			os.Exit(0)
 		}
 
-		util.Run(params)
+		status, err := worktree.Status()
+		if err != nil {
+			fmt.Println("Could not check dirtiness of branch")
+			os.Exit(0)
+		}
+
+		if !status.IsClean() {
+			fmt.Println("Dirty worktree, please commit or stash first")
+			os.Exit(0)
+		}
+
+		// First try to checkout branch
+		err = worktree.Checkout(&git.CheckoutOptions{Create: false, Force: false, Branch: b})
+
+		if err != nil {
+			// got an error  - try to create it
+			err := worktree.Checkout(&git.CheckoutOptions{Create: true, Force: false, Branch: b})
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(0)
+			}
+		}
+
+		templateRef := fmt.Sprintf("refs/heads/%s", template)
+		testBranch := &gitConfig.Branch{
+			Name:   template,
+			Remote: "origin",
+			Merge:  plumbing.ReferenceName(templateRef),
+		}
+		err = r.CreateBranch(testBranch)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(0)
+		}
 	},
 }
 
